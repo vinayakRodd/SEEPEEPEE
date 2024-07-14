6757907994eb1175d69c3e7094af780b131e522f
Vinayak Rodd <150567680+vinayakRodd@users.noreply.github.com>
2024-07-14 12:47:44 +0530

    Create KadaneAlgoWhere-1CaseSolved.cpp

---
+++ KadaneAlgoWhere-1CaseSolved.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        
+        int sum = 0;
+        int MaxSum = INT_MIN;
+        
+
+        for(int i=0; i<nums.size(); i++){
+
+            sum += nums[i];
+
+            if(sum > MaxSum)
+                MaxSum = sum;
+
+            if(sum < 0)
+                sum = 0;
+        }
+
+        return MaxSum;
+    }
+};
