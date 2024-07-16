a24da0be8d45bd921f53b250b2dd79e930b5ded6
Vinayak Rodd <150567680+vinayakRodd@users.noreply.github.com>
2024-07-16 14:16:56 +0530

    Create GoogleQuelongngestSumInSequence.cpp

---
+++ GoogleQuelongngestSumInSequence.cpp
@@ -0,0 +1,32 @@
+int longestSuccessiveElements(vector<int>&a) {
+    
+    sort(a.begin(),a.end());
+
+    int longest = 1;
+    int countCurr = 0;
+    int lastSmall = INT_MIN;
+    for(int i=0; i<a.size(); i++){
+
+        if(a[i]-1 == lastSmall){
+
+            lastSmall = a[i];
+            countCurr++;
+        }
+
+        else    
+        if(a[i] != lastSmall){
+            countCurr = 1;
+            lastSmall = a[i];
+        }
+
+        
+
+        if(countCurr > longest)
+            longest = countCurr;
+    }
+
+    
+
+    
+    return longest;
+}
