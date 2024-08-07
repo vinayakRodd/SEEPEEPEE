60c08242262dc403424a43a60be6541ce08e743a
Vinayak Rodd <150567680+vinayakRodd@users.noreply.github.com>
2024-08-07 11:39:48 +0530

    Create LowerBoundBS.cpp

---
+++ LowerBoundBS.cpp
@@ -0,0 +1,25 @@
+int lowerBound(vector<int> arr, int n, int x) {
+	
+	int low = 0,high = n-1;
+	int lb = n;                        
+	while(low<=high){
+
+		int mid = (low+high)/2;
+
+		if(x <= arr[mid]){
+			lb = mid;
+			high = mid-1;
+		}
+		else
+			low = mid+1;
+
+		
+	}
+
+  // Do not worry abou the if the element is 0 and does not exist as this algo will find the element greater than that.. and also if the element does not exist and it is the largest element then will ust return the 
+  // size of the array as the index.. hypothetically //
+	return lb;
+
+
+
+}
